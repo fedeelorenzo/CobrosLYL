2c62f67a283eb1a9562fbff45a5449b0cb78d990
fedeelorenzo <federico@estudiolorenzoasoc.com>
2025-08-12 03:04:28 -0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 # config.py
-# ⚠️ Completá estos valores antes de usar en producción
-JWT = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpZGN1aXQiOiIxNjY1NjYiLCJpZHVzdWFyaW8iOjQ0NzY5LCJzZWVkIjoyOTEzOTY1OTY3MjY2ODV9.n3c84D5ulQut789UTNE3ruRbyeDyAPEUwP6xZWX061GiHUkR0XCU0b7eD5dSOSMlsaan3jOh9Bk00YQWTax3ow"
+import streamlit as st
+
+# Lee las credenciales del archivo secrets.toml
+JWT = st.secrets["JWT"]
 
 # Solo las cuentas que vas a usar como medios de cobro (nombre visible -> ID cuenta contable)
 CUENTAS_COBRO = {
@@ -10,12 +12,7 @@ CUENTAS_COBRO = {
     "Banco Provincia SH": 78043991,
     "Banco Provincia GL/DL ": 227197490,
     "Banco ICBC Adri": 236403092,
-    # "Caja Estudio": 51384293,
-    # "Caja Dani": 51384294,
-    # "Caja Gabi": 52300001,
-    # "Banco Provincia SH": 52300001,
-    # "Banco Provincia Gabi/Dani": 52300001,
-    # "Banco ICBC Adri": 52300001,
+    "Retencion Impuesto a las Ganancias": 78043603,
     # "Banco ICBC Hector": 52300001,
     # "Banco Provincia Dani": 52300001,
     # "Banco UALA Meli": 52300001,
